1103cf71c2952ab1e59b113042bca844c878d7eb
Marcelo Marques <102492818+marcelo-mls@users.noreply.github.com>
2023-02-10 00:07:31 -0300

    fix(update): - Remove arquivo obsoleto

--- src/mock.js
+++
@@ -1,9 +0,0 @@
-const mock = [
-  { year: '1949', event: 'Revolução Chinesa', image: 'encurtador.com.br/dkmox' },
-  { year: '1953', event: 'Morte de Stalin', image: 'encurtador.com.br/epCP0' },
-  { year: 1967, event: 'Morte de Che Guevara', image: 'encurtador.com.br/bwAT6' },
-  { year: '1969', event: 'Morte de Carlos Marighella', image: 'encurtador.com.br/lmvw1' },
-  { year: 1980, event: 'Lula Funda o Partido dos Trabalhadores', image: 'encurtador.com.br/vBPU6' },
-];
-
-export default mock;
