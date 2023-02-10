427a22bc6a4c806dbe9bad8b770ab7edba27a44a
Marcelo Marques <marcelo-mls@hotmail.com>
2023-02-09 23:23:36 -0300

    fix: envolve o app com o provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import AppProvider from './context/AppProvider';
 
 import Main from './pages/Main';
 
 function App() {
   return (
-    <>
+    <AppProvider>
       <Main />
       <ToastContainer position="bottom-center" autoClose={2000} />
-    </>
+    </AppProvider>
   );
 }
 
