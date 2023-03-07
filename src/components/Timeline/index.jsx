d8dbde8abacba99e2b11a66c9ba2b7811b69f036
Marcelo Marques <marcelo-mls@hotmail.com>
2023-03-07 02:39:41 -0300

    chore: salva alterações

--- src/components/Timeline/index.jsx
+++ src/components/Timeline/index.jsx
@@ -1,4 +1,6 @@
-import React, { useContext } from 'react';
+import React, {
+  useContext, useRef, useState, useEffect,
+} from 'react';
 import PropTypes from 'prop-types';
 import { Tooltip, Zoom } from '@mui/material';
 
@@ -8,11 +10,18 @@ import AppContext from '../../context/AppContext';
 
 function Timeline() {
   const { timelineData } = useContext(AppContext);
+  const ref = useRef(null);
+  const [myWidth, setMyWidth] = useState('');
+
+  useEffect(() => {
+    const width = ref.current.offsetWidth;
+    setMyWidth(width);
+  }, [ref.current]);
 
   return (
-    <TimelineContainer>
+    <TimelineContainer myWidth={myWidth * timelineData.length}>
       {timelineData.map(({ historicalEvent, year, imageUrl }, index) => (
-        <div key={historicalEvent} className={index % 2 === 0 ? 'up' : 'down'}>
+        <div key={historicalEvent} ref={ref}>
           <YearText>{year}</YearText>
           <p>{historicalEvent}</p>
           <Tooltip
